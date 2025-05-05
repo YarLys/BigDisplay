8191503bdb577dc5cd2a347214f40a41436d84df
YarLys <yar_lysakov@mail.ru>
2025-05-05 20:04:22 +0300

    added buttons for image slider. need to add video processing

--- composeApp/src/desktopMain/kotlin/org/example/bigdisplayproject/feature/display/presentation/components/NewsDetails.kt
+++ composeApp/src/desktopMain/kotlin/org/example/bigdisplayproject/feature/display/presentation/components/NewsDetails.kt
@@ -1,53 +1,50 @@
 package org.example.bigdisplayproject.feature.display.presentation.components
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import coil3.compose.LocalPlatformContext
 import coil3.request.ImageRequest
 import io.ktor.http.headers
+import kotlinx.coroutines.launch
 import org.example.bigdisplayproject.feature.display.network.dto.Link
 import org.example.bigdisplayproject.feature.display.network.dto.News
 import org.example.bigdisplayproject.feature.display.network.dto.Photo
 
 @Composable
 fun NewsDetails(news: News, onBackButtonClick: () -> Unit) {
+
+    val scope = rememberCoroutineScope()
 
     Scaffold(
         bottomBar = { BottomPanel { onBackButtonClick() } }
@@ -91,7 +88,7 @@ fun NewsDetails(news: News, onBackButtonClick: () -> Unit) {
                         var pagerState = rememberPagerState(pageCount = { attachments.size })
                         Box(
                             modifier = Modifier
-                                //.height(400.dp)
+                                .height(450.dp)
                                 .fillMaxWidth()
                         ) {
                             HorizontalPager(
@@ -122,6 +119,48 @@ fun NewsDetails(news: News, onBackButtonClick: () -> Unit) {
                                         println("Ошибка загрузки. Проверьте URL и заголовки.")
                                     }
                                 )
+                            }
+                        }
+                        if (attachments.size > 1) {
+                            Box(
+                                modifier = Modifier
+                                    //.offset(y = -(16).dp)
+                                    .fillMaxWidth(0.3f)
+                                    .clip(RoundedCornerShape(100))
+                                    .background(MaterialTheme.colorScheme.background)
+                                    .padding(8.dp)
+                                    .align(Alignment.CenterHorizontally)
+                            ) {
+                                IconButton(
+                                    onClick = {
+                                        scope.launch {
+                                            pagerState.animateScrollToPage(
+                                                pagerState.currentPage - 1
+                                            )
+                                        }
+                                    },
+                                    modifier = Modifier.align(Alignment.CenterStart)
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                                        contentDescription = "Go back"
+                                    )
+                                }
+                                IconButton(
+                                    onClick = {
+                                        scope.launch {
+                                            pagerState.animateScrollToPage(
+                                                pagerState.currentPage + 1
+                                            )
+                                        }
+                                    },
+                                    modifier = Modifier.align(Alignment.CenterEnd)
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                                        contentDescription = "Go forward"
+                                    )
+                                }
                             }
                         }
                         Spacer(modifier = Modifier.width(16.dp))
